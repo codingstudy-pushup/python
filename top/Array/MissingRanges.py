2bc3d3236d5d59dd006a7330df5cf4f968d45c44
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-07-06 18:33:06 +0900

    Update MissingRanges.py

--- top/Array/MissingRanges.py
+++ top/Array/MissingRanges.py
@@ -2,14 +2,30 @@ from typing import List
 
 
 class Solution:
+
     def solve(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        nums = [lower - 1] + nums + [upper + 1]
+        res = []
+
+        for i in range(len(nums) - 1):
+            if nums[i + 1] - nums[i] == 2:
+                res.append(str(nums[i] + 1))
+            elif nums[i + 1] - nums[i] > 2:
+                res.append(str(nums[i] + 1) + '->' + str(nums[i + 1] - 1))
+
+        return res
+
+    def solve_2(self, nums: List[int], lower: int, upper: int) -> List[str]:
         ranges=[]
         nums=[lower-1] + nums + [upper+1]
         for i in range(len(nums)-1):
             if nums[i+1] - nums[i] == 2:
-                ranges += ["{}".format(nums[i]+1)]
+                ranges += ['{}'.format(nums[i]+1)]
             if nums[i+1] - nums[i] > 2:
                 ranges += ["{}->{}".format(nums[i]+1, nums[i+1]-1)]
         return ranges
 
+
+
 print(Solution().solve([2,3,5,50,75], 0, 99))
+print(Solution().solve_2([2,3,5,50,75], 0, 99))
