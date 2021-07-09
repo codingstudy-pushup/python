7d5097ec870a8b38d859a9be644c58b06a2708a0
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-07-09 10:47:51 +0900

    Update FindAllAnagramInString.py

--- top/TwoPointers/FindAllAnagramInString.py
+++ top/TwoPointers/FindAllAnagramInString.py
@@ -1,34 +1,48 @@
 import collections
-from typing import List
+from typing import List, Counter
 
 
 class Solution:
-    def countarr(self, w):
-        out = [0] * 26
-        for c in w:
-            out[ord(c) - ord('a')] += 1
-        return out
+    def solve_1(self, s: str, p: str) -> List[int]:
+        res = []
+        if len(s) < len(p):
+            return res
+        pPat = [0] * 26
+        sPat = [0] * 26
 
-    def solve_ord(self, s: str, p: str) -> List[int]:
+        for i in range(0, len(p)):
+            pPat[ord(p[i]) - ord("a")] += 1
+            sPat[ord(s[i]) - ord("a")] += 1
+        if pPat == sPat:
+            res.append(0)
+        for i in range(1, len(s) - len(p) + 1):
+            print('i', i)
+            sPat[ord(s[i - 1]) - ord("a")] -= 1
+            sPat[ord(s[i + len(p) - 1]) - ord("a")] += 1
+            if pPat == sPat:
+                res.append(i)
+        return res
 
-        m = len(p)
-        ps = self.countarr(p)
-        ans = []
-        ss = self.countarr(s[:m])
+    def solve_2(self, s: str, p: str) -> List[int]:
+        def makeMap(string):  # O(26)
+            bitmap = [0 for i in range(26)]
+            for c in string:
+                bitmap[ord(c) - ord('a')] += 1
+            return bitmap
 
-        if ss == ps:
-            ans = [0]
+        pLength = len(p)
+        pPat = makeMap(p)
+        sPat = makeMap(s[:pLength])
+        res = []
+        if sPat == sPat:
+            res.append(0)
 
-        for i in range(1, len(s) - m + 1):
-
-            left = ord(s[i - 1]) - ord('a')
-            right = ord(s[i + m - 1]) - ord('a')
-            ss[left] = ss[left] - 1
-            ss[right] = ss[right] + 1
-
-            if ss == ps:
-                ans.append(i)
-        return ans
+        for i in range(1, len(s) - pLength + 1):
+            sPat[ord(s[i - 1]) - ord('a')] -= 1
+            sPat[ord(s[i + pLength - 1]) - ord('a')] += 1
+            if sPat == pPat:
+                res.append(i)
+        return res
 
     def solve(self, s: str, t: str) -> List[int]:
         d = collections.Counter(t)
