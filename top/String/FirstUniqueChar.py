d969ff1a016bcc989b4b30cc10e87ff1028e8da2
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-07-22 05:08:09 +0900

    Create FirstUniqueChar.py

---
+++ top/String/FirstUniqueChar.py
@@ -0,0 +1,39 @@
+import collections
+
+
+class Solution:
+    def solve1(self, s: str) -> int:
+        ch=[]
+        s2 = set(s)
+        print(s2)
+        for i in set(s):
+            if s.count(i) == 1:
+                ch.append(s.index(i))
+        if len(ch)>0:
+            return min(ch)
+        else:
+            return -1
+
+    def solve2(self, s: str) -> int:
+        if not s: return -1
+        d = collections.defaultdict(int)
+
+        for char in s:
+            d[char] += 1
+
+        for i, c in enumerate(s):
+            if d[c] < 2:
+                return i
+        return -1
+
+    def solve3(self, s):
+
+        from collections import Counter
+        s_count = Counter(s)
+        for x in s_count:
+            if s_count[x] == 1:
+                return s.index(x)
+        return -1
+
+print(Solution().solve1('lovelo'))
+print(Solution().solve3('lovelo'))
